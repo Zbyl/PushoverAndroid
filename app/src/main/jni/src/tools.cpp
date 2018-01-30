99cd3e2ba0153a35aacf33d1a91b0aa2ba3fd53b
Zbigniew Skowron <zbychs@gmail.com>
2018-01-30 22:26:36 +0100

    Hard coded list of all original levels.

--- app/src/main/jni/src/tools.cpp
+++ app/src/main/jni/src/tools.cpp
@@ -70,6 +70,111 @@ std::string getHome() {
   return prefDirString;
 }
 
+const char* levels[] = {
+  "001.level",
+  "002.level",
+  "003.level",
+  "004.level",
+  "005.level",
+  "006.level",
+  "007.level",
+  "008.level",
+  "009.level",
+  "010.level",
+  "011.level",
+  "012.level",
+  "013.level",
+  "014.level",
+  "015.level",
+  "016.level",
+  "017.level",
+  "018.level",
+  "019.level",
+  "020.level",
+  "021.level",
+  "022.level",
+  "023.level",
+  "024.level",
+  "025.level",
+  "026.level",
+  "027.level",
+  "028.level",
+  "029.level",
+  "030.level",
+  "031.level",
+  "032.level",
+  "033.level",
+  "034.level",
+  "035.level",
+  "036.level",
+  "037.level",
+  "038.level",
+  "039.level",
+  "040.level",
+  "041.level",
+  "042.level",
+  "043.level",
+  "044.level",
+  "045.level",
+  "046.level",
+  "047.level",
+  "048.level",
+  "049.level",
+  "050.level",
+  "051.level",
+  "052.level",
+  "053.level",
+  "054.level",
+  "055.level",
+  "056.level",
+  "057.level",
+  "058.level",
+  "059.level",
+  "060.level",
+  "061.level",
+  "062.level",
+  "063.level",
+  "064.level",
+  "065.level",
+  "066.level",
+  "067.level",
+  "068.level",
+  "069.level",
+  "070.level",
+  "071.level",
+  "072.level",
+  "073.level",
+  "074.level",
+  "075.level",
+  "076.level",
+  "077.level",
+  "078.level",
+  "079.level",
+  "080.level",
+  "081.level",
+  "082.level",
+  "083.level",
+  "084.level",
+  "085.level",
+  "086.level",
+  "087.level",
+  "088.level",
+  "089.level",
+  "090.level",
+  "091.level",
+  "091_snes.level",
+  "092.level",
+  "093.level",
+  "094.level",
+  "095.level",
+  "096.level",
+  "097.level",
+  "098.level",
+  "099.level",
+  "101.level",
+  "100.level",
+};
+
 std::vector<std::string> directoryEntries(const std::string & path) {
   std::vector<std::string> entries;
 
@@ -78,9 +183,9 @@ std::vector<std::string> directoryEntries(const std::string & path) {
   }
 
   if (path == "pushover/levels/original") {
-    entries.push_back("001.level");
-    entries.push_back("002.level");
     entries.push_back("index.level");
+    for (auto level : levels)
+      entries.push_back(level);
   }
 
   return entries;
@@ -168,4 +273,3 @@ void amigaLocale(void)
   }
 #endif
 }
-
